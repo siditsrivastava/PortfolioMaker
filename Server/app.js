2b51097ce73136eee7ad92d018fbbbed3de44f84
Sidit Srivastava <siditsrivastava84@gmail.com>
2024-04-28 23:45:59 +0530

    AddNEWThing

--- Server/app.js
+++ Server/app.js
@@ -1,19 +1,17 @@
+require('dotenv').config()
 const express = require("express");
-const cors = require("cors");
-
-
-
+// const cors = require("cors");
 
 const app = express();
 
+const PORT = 4000
 
-app.use(cors());
-
-
-app.listen(5000 , () => {
-    console.log("sidit")
+// app.use(cors());
+app.get("/registation", (req, res) => {
+  res.send(`sidit`);
+});
+// console.log(process.env.PORT)
+app.listen(process.env.PORT, () => {
+  console.log("sidit");
 });
 
-app.get("/" ,(req, res) => {
-    res.send("Sever is Started !!");
-})
